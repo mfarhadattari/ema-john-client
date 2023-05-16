3126dfc1d7876281a633070b22276798edd09f1d
mfarhadattari <mfarhad.attari@gmail.com>
2023-05-17 00:09:18 +0600

    error page resplonsive

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,8 +1,8 @@
 const Error = () => {
   return (
-    <div className="text-6xl text-slate-900 flex justify-center gap-10 mt-48 font-light">
-      <div>404</div>
-      <div className="border-s-4 border-yellow-500 ps-5">
+    <div className="text-6xl text-slate-900 flex flex-col md:flex-row justify-center gap-10 mt-48 font-light">
+      <div className="text-center">404</div>
+      <div className="md:border-s-4 border-yellow-500 md:ps-5 text-center text-3xl md:text-6xl">
         Nothing here to see
       </div>
     </div>
